706e8496f9074a1bf173f1f92c51117ec4ba2c2a
Flan <francisjgrande@gmail.com>
2019-03-23 11:57:46 +0000

    More jump development
    Getting there.

--- scripts/curilla2D.gd
+++ scripts/curilla2D.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 
 onready var jump_timer = get_node("jump_timer")
 
+# general variables
+onready var window = get_tree().get_root();
+var spawn = Vector2(320, 480);
+
 # camera variables
 var cam_target_position = Vector2(0.0, 0.0) #Position the camera is moving to
 var cam_max_range = Vector2(46, 46) #Max distance from the character to the camera. Not used for now. Maximum recommended is about 50. Minimum 0.
@@ -9,16 +13,18 @@ onready var camera = get_node("camera")
 
 # movement variables
 const MAX_SPEED_WALK = Vector2(3000, 1000) #Character maximum allowed speed when walking
-const MAX_SPEED_RUN = Vector2(12000, 1000) #Character maximum allowed speed when running
+const MAX_SPEED_RUN = Vector2(18000, 1000) #Character maximum allowed speed when running
+const MAX_SPEED_JUMP = Vector2(24000, 1000) #Character maximum allowed speed when jumping
 var max_speed = MAX_SPEED_WALK #Current max_speed
-var char_acceleration = Vector2(1000, 700) #Character acceleration
-var char_jump_acceleration = Vector2(1600, 400) #Character acceleration
-var jump_speed = 8000
+var char_acceleration = Vector2(400, 700) #Character acceleration
+var char_jump_acceleration = Vector2(200, 400) #Character acceleration
+var jump_speed = Vector2(18000, 8000)
 var char_speed = Vector2(0.0, 0.0) #Current character speed
 var right = true #Direction character is facing
 var input_x = false #Player is not making any input on x axis
 var input_y = false #Player is not making any input on y axis
 var jumping = false
+var falling = true
 
 # aiming system variables
 var priest_shoulder #Point of origin from where the aiming system takes the angles. A sweet spot between shoulders and head, probably.
@@ -83,6 +89,10 @@ func _process(delta):
 	var motion = char_speed * delta
 	print(motion)
 	move_and_slide(motion, Vector2(0, -1))
+	
+	if(position.y > window.size.y):
+		respawn()
+	
 	pass
 
 #Controls the input in an abstract way
@@ -91,7 +101,7 @@ func move_input(delta):
 	movement_checks() #Move left or right. Is character running?
 	aim_checks() #Aim in 360 degrees.
 	camera_checks() #Move camera when aiming.
-	shoot_checks()	#PUM PUM!
+	shoot_checks() #PUM PUM!
 	
 	#Max x speed
 	if (abs(char_speed.x) > max_speed.x):
@@ -109,7 +119,9 @@ func movement_checks():
 			if(Input.is_action_pressed("run")):
 				if (Input.is_action_pressed("ui_right") or (Input.is_action_pressed("ui_left"))):
 					change_anim("horizontal_jump")
-					char_speed.y = -1 * jump_speed
+					#char_speed.x = sign(char_speed.x) * jump_speed.x
+					max_speed = MAX_SPEED_JUMP
+					char_speed.y = -1 * jump_speed.y
 					jumping = true
 					jump_timer.start();
 		else:
@@ -152,9 +164,17 @@ func movement_checks():
 func air_checks():
 	char_speed.y += char_acceleration.y
 	
+	if(jumping):
+		char_speed.x += sign(char_speed.x) * char_jump_acceleration.x
+	
 	if(test_move(get_transform(), Vector2(0, 1))):
 		char_speed.y = 0
 		jumping = false
+		falling = false
+	else:
+		if(not jumping):
+			change_anim("falling")
+			falling = true
 
 func aim_checks():
 	if(Input.is_action_pressed("aim")):
@@ -223,4 +243,7 @@ func change_anim(newanim):
 		get_node("AnimationPlayer").play(newanim) #Change it!
 
 func is_character_idle():
-	return not input_x and not input_y and not Input.is_action_pressed("aim")
+	return not input_x and not input_y and not Input.is_action_pressed("aim") and not falling
+
+func respawn():
+	position = spawn
